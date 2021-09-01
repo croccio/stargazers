4ddcb69c1ec74dbe0186d00218ebdd442fa88e5b
antonioscardigno <cwz.scardigno@gmail.com>
2021-09-02 00:00:38 +0200

    #2 Create a singleton rest api class

---
+++ LocalPods/stargazers-service/stargazers-service/Classes/lib/StargazersServiceConfig.swift
@@ -0,0 +1,42 @@
+//
+// Created by Antonio Scardigno on 2021-09-01.
+//
+
+import Foundation
+
+public struct StargazersServiceConfig {
+
+    static let BASE_URL = "https://api.github.com"
+
+}
+
+
+public protocol BaseService {
+    var type: ServiceType { get }
+    var url: String { get }
+    var body: String? { get }
+}
+
+public enum ServiceType: String {
+    case GET = "GET",
+         POST = "POST",
+         PUT = "PUT",
+         PATCH = "PATCH",
+         DELETE = "DELETE"
+}
+
+
+public class StargazersRequest {
+
+    public static func getRequest(service: BaseService) -> URLRequest {
+        let url = URL(string: service.url)!
+        var request = URLRequest(url: url)
+        request.httpMethod = service.type.rawValue
+        
+        if let body = service.body {
+            request.httpBody = body.data(using: .utf8)
+        }
+
+        return request
+    }
+}
---
+++ LocalPods/stargazers-service/stargazers-service/Classes/lib/service/StargazersApi.swift
@@ -0,0 +1,14 @@
+//
+// Created by Antonio Scardigno on 2021-09-01.
+//
+
+import Foundation
+
+public class StargazersApi {
+
+    public let instance = StargazersApi()
+
+    init () {}
+
+
+}
